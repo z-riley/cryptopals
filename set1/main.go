84c5b79ab8fcf09964345427b4637bd27c5e20d4
zac460 <zac460@hotmail.co.uk>
2024-06-24 23:32:06 +0100

    Add fixed XOR

--- set1/main.go
+++ set1/main.go
@@ -13,3 +13,21 @@ func HexToBase64(s string) []byte {
 	}
 	return []byte(base64.StdEncoding.EncodeToString(b))
 }
+
+// 2. Fixed XOR
+func FixedXOR(s string, xor string) []byte {
+	b, err := hex.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	bxor, err := hex.DecodeString(xor)
+	if err != nil {
+		panic(err)
+	}
+
+	buf := make([]byte, len(b))
+	for i := range b {
+		buf[i] = b[i] ^ bxor[i]
+	}
+	return []byte(hex.EncodeToString(buf))
+}
--- set1/main_test.go
+++ set1/main_test.go
@@ -14,3 +14,15 @@ func TestHexToBase64(t *testing.T) {
 	}
 
 }
+
+func TestFixedXOR(t *testing.T) {
+	expected := []byte("746865206b696420646f6e277420706c6179")
+	actual := FixedXOR(
+		"1c0111001f010100061a024b53535009181c",
+		"686974207468652062756c6c277320657965",
+	)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected: %s\nGot: %s", expected, actual)
+	}
+}
