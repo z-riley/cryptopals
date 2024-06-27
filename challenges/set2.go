0503fe298cbe25634cb557ee543c07dca22f5025
zac460 <zac460@hotmail.co.uk>
2024-06-27 11:51:47 +0100

    Complete challenge 9

--- challenges/set2.go
+++ challenges/set2.go
@@ -1,1 +1,15 @@
 package challenges
+
+import (
+	"strings"
+)
+
+// 9. Implement PKCS#7 padding
+func PadPKCS7(s string, blockSize int) string {
+	if len(s) > blockSize {
+		panic("PadPKCS7() string cannot be larger than the block size")
+	}
+	numPadding := blockSize - len(s)
+	padded := s + strings.Repeat(string(rune(numPadding)), numPadding)
+	return padded
+}
--- challenges/set2_test.go
+++ challenges/set2_test.go
@@ -1,1 +1,11 @@
 package challenges
+
+import "testing"
+
+func TestPadPKCS7(t *testing.T) {
+	expected := "YELLOW SUBMARINE\x04\x04\x04\x04"
+	actual := PadPKCS7("YELLOW SUBMARINE", 20)
+	if expected != actual {
+		t.Errorf("Expected: %v\nGot: %v\n", expected, actual)
+	}
+}
